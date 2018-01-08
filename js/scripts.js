974492b76e356a0d127eeab2504bcc50496d2edf
Hannah Lee and Ian Goodrich <hannah@>
2018-01-08 10:32:17 -0800

    Add BMI function.

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,19 @@ var num1 = parseInt(prompt("Give me a number:"));
 var num2 = parseInt(prompt("Give me another number:"));
 
 alert(divide(num1,num2));
+
+
+
+
+var BMI = function () {
+  weight = parseFloat(prompt("What is your weight?"));
+  height = parseFloat(prompt("What is your height?"));
+  var weight_mod = weight * .45;
+  var height_mod = height * 0.025;
+  var height_modsq = height_mod * height_mod;
+  var answer = weight_mod / height_modsq;
+
+  alert(answer);
+}
+
+BMI()
